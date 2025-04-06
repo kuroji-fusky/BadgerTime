43980314f5e1d0409cded542eb3b6d0344ddf7ca
Kerby Keith Aquino <hello@kurojifusky.com>
2025-04-06 19:05:29 +0800

    a lil refactoring

--- web/src/lib/components/Portal.svelte
+++ web/src/lib/components/Portal.svelte
@@ -13,18 +13,18 @@
   }
 
   onMount(() => {
-    if (import.meta.env.DEV) {
-      if (!window.customElements.get("portal-detached")) {
-        // svelte-ignore perf_avoid_nested_class
-        class DudPortalElement extends HTMLDivElement {}
-
-        window.customElements.define("portal-detached", DudPortalElement, {
-          extends: "div",
-        });
-      } else {
-        console.warn("Custom element 'portal-detached' is already defined.");
-      }
+    if (!import.meta.env.DEV) return;
+    if (window.customElements.get("portal-detached")) {
+      console.warn("Custom element 'portal-detached' is already defined.");
+      return;
     }
+
+    // svelte-ignore perf_avoid_nested_class
+    class DudPortalElement extends HTMLDivElement {}
+
+    window.customElements.define("portal-detached", DudPortalElement, {
+      extends: "div",
+    });
   });
 
   const { location, children }: Props = $props();
